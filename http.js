a7599ccc521ca993870552b511f98ebdf369d3a7
Zeljko Simic <zeljkosbgd@gmail.com>
2015-02-25 15:44:34 +0100

    jquery ajax wrapper

---
+++ http.js
@@ -0,0 +1,50 @@
+;
+define(function(){
+
+    var Http = function(options){
+        this.response = null;
+        this.options = $.extend(this.getDefaults(), options);
+        this.call();
+    };
+
+    Http.prototype = {
+
+        getDefaults: function()
+        {
+            return {
+                url: "",
+                type: "GET",
+                data: "",
+                dataType: "json"
+            }
+        },
+
+        call: function()
+        {
+            this.response = $.ajax({
+                type:this.options.type,
+                url: this.options.url,
+                dataType: this.options.dataType,
+                data: this.options.data
+            });
+
+            return this;
+        },
+
+        error: function(callback)
+        {
+            this.response.error(callback);
+            return this;
+        },
+
+        success: function(callback)
+        {
+            this.response.done(callback);
+            return this;
+        }
+
+    };
+
+    return Http;
+
+});
